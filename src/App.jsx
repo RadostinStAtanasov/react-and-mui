f71f4aa370bba2cba14f0555c7096aeb1a63dd16
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2025-03-20 11:54:11 +0200

    install mui librarary

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <h1>React Router Workshop</h1>
+      <Button variant="contained">Button</Button>
+    </>
+  )
+}
+
+export default App
