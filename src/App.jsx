ddb56168c72198c0392b0dc93a45d9ba25562699
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2025-03-21 12:23:20 +0200

    add register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Home from './components/Home';
 import Pricing from './components/Pricing';
+import LogIn from './components/Login';
+import SignUp from './components/SignUp';
+
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/pricing' element={<Pricing />}/>
+        <Route path='/login' element={<LogIn />}/>
+        <Route path='/register' element={<SignUp />}/>
       </Routes>
     </>
   )
